46a6a2405ccb0e4b88cc14b782f2605c1cf67809
pbohdanov <107268135+pbohdanov@users.noreply.github.com>
2023-11-30 00:16:44 +0300

    commit 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector(".gallery");
+
+const renderList = (arr, container) =>{ 
+    const markup = arr.map((item) => `<li class="gallery_item"> 
+    <a class="gallery_link" href="${item.original}">
+    <img
+        class="gallery_image"
+        src="${item.preview}"
+        data-source ="${item.original}"
+        alt="${item.description}"
+        width="360"
+        />
+    </a>
+    </li>`).join("");
+    
+    container.insertAdjacentHTML("afterbegin", markup);
+}
+renderList(galleryItems, listEl);
+
+  new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionType: 'attr', 
+        captionsData: 'alt', 
+        animationSpeed: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,73 @@
+import throttle from 'lodash.throttle';
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+const form = document.querySelector('.feedback-form');
+
+const emailInput = document.querySelector('input');
+const messageInput = document.querySelector('textarea');
+
+
+form.addEventListener("input", throttle(savedUserData), 500);
+(throttle,updatePage(), 500);
+form.addEventListener("submit", cleanedStorage );
+
+
+
+function savedUserData(evt) {
+    evt.preventDefault();
+
+   const { email, message } = evt.currentTarget.elements;
+
+    if (email.value === "" || message.value === "") {
+        console.log("Please fill in all the fields!");
+        return;
+    }
+
+        const info = {
+            email: email.value,
+            message: message.value
+        }
+
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(info)); 
+
+}
+
+
+function updatePage() {
+
+    const storedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+    if (storedData) {
+
+        const { email, message } = storedData;
+        emailInput.value = email;
+        messageInput.value = message;
+
+    } else { 
+
+        emailInput.value = "";
+        messageInput.value = "";
+    }
+
+}
+
+
+function cleanedStorage(evt) {
+    const storedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+    if (storedData) {
+        const { email, message } = storedData;
+
+        if (emailInput.value === email && messageInput.value === message) {
+
+
+            localStorage.removeItem(LOCALSTORAGE_KEY);
+            evt.currentTarget.reset();
+
+        }
+
+    }       
+
+}
 
