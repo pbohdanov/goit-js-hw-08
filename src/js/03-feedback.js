26ae886050eb98353b1ed93254264453199f0c87
pbohdanov <p.bohdanov@gmail.com>
2023-12-02 07:41:14 +0300

    update 03 feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,41 +3,15 @@ import throttle from 'lodash.throttle';
 const LOCALSTORAGE_KEY = "feedback-form-state";
 
 const form = document.querySelector('.feedback-form');
-
 const emailInput = document.querySelector('input');
 const messageInput = document.querySelector('textarea');
 
 
-form.addEventListener("input", throttle(savedUserData), 500);
-(throttle,updatePage(), 500);
-form.addEventListener("submit", cleanedStorage );
+// Load stored data from localStorage
 
+const storedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
-
-function savedUserData(evt) {
-    evt.preventDefault();
-
-   const { email, message } = evt.currentTarget.elements;
-
-    if (email.value === "" || message.value === "") {
-        console.log("Please fill in all the fields!");
-        return;
-    }
-
-        const info = {
-            email: email.value,
-            message: message.value
-        }
-
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(info)); 
-
-}
-
-
-function updatePage() {
-
-    const storedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-
+// Fill form fields with stored data if available
     if (storedData) {
 
         const { email, message } = storedData;
@@ -49,25 +23,48 @@ function updatePage() {
         emailInput.value = "";
         messageInput.value = "";
     }
+    
+// Throtlle the saveUserData function using lodash.throttle
 
-}
+const throttledSaveUserData = throttle(savedUserData, 500);
 
+// Attach event listeners
+form.addEventListener("input", savedUserData);
+form.addEventListener("submit", cleanedStorage);
 
-function cleanedStorage(evt) {
-    const storedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+// Save user data to localStorage
+function savedUserData(evt) {
+    evt.preventDefault();
 
-    if (storedData) {
-        const { email, message } = storedData;
-
-        if (emailInput.value === email && messageInput.value === message) {
-
-
-            localStorage.removeItem(LOCALSTORAGE_KEY);
-            evt.currentTarget.reset();
-
+   const { email, message } = evt.currentTarget.elements;
+    
+        const info = {
+            email: email.value,
+            message: message.value
         }
-
-    }       
-
+        
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(info)); 
 }
 
+//Clean storage and resert form on submission
+function cleanedStorage(evt) {
+    evt.preventDefault();
+
+    const { email, message } = evt.currentTarget.elements;
+    
+      if (email.value === "" || message.value === "") {
+          alert("Please fill in all the fields!");
+          return;
+        
+      }
+    
+    const cleanedData = {
+        email: email.value,
+        message: message.value,
+    }
+
+        localStorage.removeItem(LOCALSTORAGE_KEY);
+        console.log(cleanedData);
+        evt.currentTarget.reset();            
+
+}
